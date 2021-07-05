48cc9ab7103b081f6bb171cc6b48469ec665f160
shinys88 <sinyusu129@gmail.com>
2021-07-05 18:59:48 +0900

    Photo_Admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import mark_safe
 from . import models
 
 
@@ -13,10 +14,17 @@ class ItemAdmin(admin.ModelAdmin):
         return obj.rooms.count()
 
 
+# class PhotoInline(admin.StackedInline):
+class PhotoInline(admin.TabularInline):
+    model = models.Photo
+
+
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
 
     """Room Admin Definition"""
+
+    inlines = (PhotoInline,)
 
     fieldsets = (
         (
@@ -66,6 +74,8 @@ class RoomAdmin(admin.ModelAdmin):
         "country",
     )
 
+    raw_id_fields = ("host",)
+
     search_fields = ("=city", "^host__username")
 
     filter_horizontal = (
@@ -90,7 +100,14 @@ class PhotoAdmin(admin.ModelAdmin):
 
     """Photo Admin Definition"""
 
-    pass
+    list_display = ("__str__", "get_thumbnail")
+
+    def get_thumbnail(self, obj):
+        # print(dir(obj.file))
+        # print(obj.file.url)
+        return mark_safe(f'<img width="50px;" src="{obj.file.url}" />')
+
+    get_thumbnail.short_description = "Thumbnail"
 
 
 # Register your models here.
