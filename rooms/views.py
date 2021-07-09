30088d63055f46f17a3338c3556ab6db52412c69
shinys88 <sinyusu129@gmail.com>
2021-07-09 16:56:30 +0900

    add templates

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,19 @@
+from datetime import datetime
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
 
-# Create your views here.
+
+def all_rooms_test(request):
+    # print(vars(request))
+    now = datetime.now()
+    hungry = True
+    # return HttpResponse(content=f"hello{now}")
+    return render(
+        request, "all_rooms_test.html", context={"now": now, "hungry": hungry}
+    )
+
+
+def all_rooms(request):
+    all_rooms = models.Room.objects.all()
+    return render(request, "rooms/home.html", context={"potato": all_rooms})
