4ade9b53c30cc763ecb14b3f147b58a1af23b963
shinys88 <sinyusu129@gmail.com>
2021-07-21 17:24:40 +0900

    add Django forms & Search

--- rooms/views.py
+++ rooms/views.py
@@ -2,7 +2,7 @@ from math import ceil
 from datetime import datetime
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from . import models
+from . import models, forms
 
 
 # 1. render 실습 : 화면 테스트
@@ -115,7 +115,10 @@ class RoomDetail(DetailView):
 from django_countries import countries
 
 
-def search(request):
+
+
+# 6-1. 검색기능 직접 만들기.
+def search1(request):
 
     # print(request.GET)
     city = request.GET.get("city", "Anywhere")
@@ -128,8 +131,8 @@ def search(request):
     bedrooms = int(request.GET.get("bedrooms", 0))
     beds = int(request.GET.get("beds", 0))
     baths = int(request.GET.get("baths", 0))
-    instant = request.GET.get("instant", False)
-    super_host = request.GET.get("super_host", False)
+    instant = bool(request.GET.get("instant", False))
+    superhost = bool(request.GET.get("superhost", False))
     s_amenities = request.GET.getlist("amenities")
     s_facilities = request.GET.getlist("facilities")
     
@@ -147,7 +150,7 @@ def search(request):
         "s_amenities": s_amenities,
         "s_facilities": s_facilities,
         "instant": instant,
-        "super_host": super_host
+        "superhost": superhost
     }
 
 
@@ -162,8 +165,136 @@ def search(request):
         "facilities": facilities,
     }
 
+
+    filter_args = {}
+
+    # if price != 0:
+    #     qs.filter(price__lte=price)
+
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
+
+    filter_args["country"] = country
+
+    if room_type != 0 :
+        # filter_args["room_type__pk__exact"] = room_type
+        filter_args["room_type__pk"] = room_type
+
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    if instant is True:
+        filter_args["instant_book"] = True
+
+    # Room Objects의 host => Forenkey => superhost
+    if superhost is True:
+        filter_args["host__superhost"] = True
+
+    if len(s_amenities) > 0:
+        for s_amenity in s_amenities:
+            filter_args["amenities__pk"] = int(s_amenity)
+
+    if len(s_facilities) > 0:
+        for s_facility in s_facilities:
+            filter_args["facilities__pk"] = int(s_facility)
+
+
+
+
+    rooms = models.Room.objects.filter(**filter_args)
+
+
+    
+
+
     return render(
         request,
-        "rooms/search.html",
-        {**form, **choices},
+        "rooms/search1.html",
+        {**form, **choices, "rooms":rooms},
     )
+
+
+
+# 6-2. 검색기능 Django forms 기능 활용하여 만들기.
+def search(request):
+
+    country = request.GET.get("country")
+
+    if country:
+
+        form = forms.SearchForm(request.GET)
+        if form.is_valid():
+            print(form.cleaned_data)
+
+            city = form.cleaned_data.get("city")
+            country = form.cleaned_data.get("country")
+            price = form.cleaned_data.get("price")
+            room_type = form.cleaned_data.get("room_type")
+            price = form.cleaned_data.get("price")
+            guests = form.cleaned_data.get("guests")
+            bedrooms = form.cleaned_data.get("bedrooms")
+            beds = form.cleaned_data.get("beds")
+            baths = form.cleaned_data.get("baths")
+            instant_book = form.cleaned_data.get("instant_book")
+            superhost = form.cleaned_data.get("superhost")
+            amenities = form.cleaned_data.get("amenities")
+            facilities = form.cleaned_data.get("facilities")
+
+
+            filter_args = {}
+
+            if city != "Anywhere":
+                filter_args["city__startswith"] = city
+
+            filter_args["country"] = country
+
+            if room_type is not None:
+                filter_args["room_type"] = room_type
+
+            if price is not None:
+                filter_args["price__lte"] = price
+
+            if guests is not None:
+                filter_args["guests__gte"] = guests
+
+            if bedrooms is not None:
+                filter_args["bedrooms__gte"] = bedrooms
+
+            if beds is not None:
+                filter_args["beds__gte"] = beds
+
+            if baths is not None:
+                filter_args["baths__gte"] = baths
+
+            if instant_book is True:
+                filter_args["instant_book"] = True
+
+            if superhost is True:
+                filter_args["host__superhost"] = True
+
+            for amenity in amenities:
+                filter_args["amenities"] = amenity
+
+            for facility in facilities:
+                filter_args["facilities"] = facility
+
+            rooms = models.Room.objects.filter(**filter_args)
+
+    else:
+        form = forms.SearchForm()
+
+    # unbound form
+
+    return render(request, "rooms/search.html", {"form":form, "rooms":rooms})
